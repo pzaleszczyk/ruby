1796c0d86862a0ec400e9421c51369f5bd9eb82d
Suchy-Laptop <Suchy-Laptop@LAPTOP-BMKRSQB4>
2019-10-28 12:21:03 +0100

    fix

--- spec/two_fer_test.rb
+++ spec/two_fer_test.rb
@@ -3,12 +3,12 @@ require_relative '../lib/two_fer.rb'
 RSpec.describe HelloWorld do
 
 	it "No name given" do
-		expect(TwoFerTest.two_fer()).to eq("One for you, one for me.")
+		expect(TwoFer.two_fer()).to eq("One for you, one for me.")
 	end
 	it "Alice" do
-		expect(TwoFerTest.two_fer("Alice")).to eq("One for Alice, one for me.")
+		expect(TwoFer.two_fer("Alice")).to eq("One for Alice, one for me.")
 	end
 	it "Bob" do
-		expect(TwoFerTest.two_fer("Bob")).to eq("One for Bob, one for me.")
+		expect(TwoFer.two_fer("Bob")).to eq("One for Bob, one for me.")
 	end
 end
