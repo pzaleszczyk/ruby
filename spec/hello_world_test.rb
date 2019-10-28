6e15acee27e6d1f82c5728826ea642d8654489f9
Suchy-Laptop <Suchy-Laptop@LAPTOP-BMKRSQB4>
2019-10-28 11:56:43 +0100

    Spec

---
+++ spec/hello_world_test.rb
@@ -0,0 +1,7 @@
+require_relative "../lib/hello_world.rb"
+
+RSpec.describe HelloWorld do
+    it "Testowanie - Hello, World!" do
+            expect(HelloWorld.hello).to eq("Hello, World!")
+    end
+end
