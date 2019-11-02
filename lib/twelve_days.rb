e89fc6398e9b3fd84600e50259caa3117eab773b
Pawel <moonster1j@gmail.com>
2019-11-02 14:44:36 +0100

    fix

--- lib/twelve_days.rb
+++ lib/twelve_days.rb
@@ -1,31 +1,25 @@
-# Sing the Twelve Days of Christmas
 class TwelveDays
-  VERSION = 1
-  PREFIX = "On the %{nth} day of Christmas my true love gave to me, %{items}\n"
-  LYRICS = {
-    first: 'a Partridge in a Pear Tree.',
-    second: 'two Turtle Doves',
-    third: 'three French Hens',
-    fourth: 'four Calling Birds',
-    fifth: 'five Gold Rings',
-    sixth: 'six Geese-a-Laying',
-    seventh: 'seven Swans-a-Swimming',
-    eighth: 'eight Maids-a-Milking',
-    ninth: 'nine Ladies Dancing',
-    tenth: 'ten Lords-a-Leaping',
-    eleventh: 'eleven Pipers Piping',
-    twelfth: 'twelve Drummers Drumming'
-  }
-  DAYS = LYRICS.keys
+  DAYS = ['first', 'second', 'third', 'fourth', 'fivth', 'sixth', 'seventh', 'eighth', 'nineth', 'tenth', 'eleventh', 'twelfth']
+  VERSES = ['and a Partridge in a Pear Tree.', 'two Turtle Doves,', 'three French Hens,', 'four Calling Birds,', 'five Gold Rings,', 'six Geese-a-Laying,', 'seven Swans-a-Swimming,', 'eight Maids-a-Milking,', 'nine Ladies Dancing,', 'ten Lords-a-Leaping,', 'eleven Pipers Piping,', 'twelve Drummers Drumming,']
 
   def self.song
-    (0...DAYS.size).map { |i| verse(i) }.join("\n")
-  end
+    array = []
+    DAYS.each_with_index do | val, index|
+      array << index
+      verse_sum = array.map do |a|
+        VERSES[a]
+      end
+      insert = verse_sum.reverse.join(' ')
 
-  def self.verse(n)
-    items = n.downto(0).map { |i| LYRICS[DAYS[i]] }
-    items[-1] = items[-1].sub(/^/, 'and ') if n > 0
-    items_phrase = items.join(', ')
-    PREFIX % { nth: DAYS[n], items: items_phrase }
+      if index.zero?
+        puts "On the #{DAYS[index]} day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+        puts ""
+      else
+        puts "On the #{DAYS[index]} day of Christmas my true love gave to me: #{insert} "
+        puts ""
+      end
+    end
   end
 end
+
+TwelveDays.song
