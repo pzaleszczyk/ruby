5501ed52620f5a503317f48bcbdce252d67fa808
Pawel <moonster1j@gmail.com>
2019-11-02 14:23:33 +0100

    Added tests/libs

--- lib/book_store.rb
+++ lib/book_store.rb
@@ -1,57 +1,41 @@
 class BookStore
-  SINGLE_PRICE = 8
-  DISCOUNT_MAP = {1 => 0.0, 2 => 0.05, 3 => 0.1, 4 => 0.2, 5 => 0.25}
-
-  def self.calculate_price(book_numbers)
-    new(book_numbers).calculate_price
-  end
-
-  def initialize(book_numbers)
-    @book_numbers = book_numbers
-  end
-
-  def calculate_price
-    return 0 if book_numbers.size == 0
-    combinations.map { |combination| total_price(combination) }.min
-  end
-
-  private
-
-  attr_reader :book_numbers
-
-  def combinations
-    [tall_combinations, wide_combinations]
-  end
-
-  def tall_combinations
-    book_number_groups.map { |group| Array.new(number_of_sets) { |i| group[i] } }.transpose.map(&:compact)
-  end
-
-  def wide_combinations
-    sets = Array.new(number_of_sets) { [] }
-    indicies = (0...sets.size).cycle
-    current_index = indicies.first
-    book_numbers.each do |number|
-      current_index = indicies.next while sets[current_index].include?(number)
-      sets[current_index] << number
-      current_index = indicies.next
+    @@book_price = 8.00
+    
+    def self.calculate_price(cart)
+      total = 0.00
+      return total if cart.empty?
+      @totals = []
+      hash = Hash.new(0)
+      cart.each{ |book| hash[book] += 1 }
+      evaluate(hash.values, total)
+      @totals.min
     end
-    sets
+  
+    def self.evaluate(books, total)
+      new_cart = books.sort.reverse
+      count = books.count{ |amount| amount > 0 }
+      if count == 0
+        @totals << total
+      else
+        for number in 1..count
+          new_cart[number-1] -= 1
+          evaluate(new_cart, total + discounted_price(number))
+        end
+      end
+    end
+  
+    def self.discounted_price(amount)
+      total = @@book_price * amount
+      if amount == 5
+        total *= 0.75
+      elsif amount == 4
+        total *= 0.80
+      elsif amount == 3
+        total *= 0.90
+      elsif amount == 2
+        total *= 0.95
+      elsif amount == 1
+        total
+      end
+    end
   end
-
-  def number_of_sets
-    book_number_groups.map(&:size).max
-  end
-
-  def book_number_groups
-    @book_number_groups ||= book_numbers.group_by(&:itself).values
-  end
-
-  def total_price(combination)
-    combination.map { |set| price(set) }.sum
-  end
-
-  def price(set)
-    set.size * SINGLE_PRICE * (1 - DISCOUNT_MAP[set.size])
-  end
-end
